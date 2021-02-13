946fafe435668b4daa05070307f0b67c54b890f3
SashaPlavi <sasha.bingulac@gmail.com>
2021-02-13 10:02:48 +0100

    action reducer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import Posts from './componets/Posts';
 import PostForm from './componets/PostForm';
+import { Provider } from 'react-redux';
+
+import { store } from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">React Redux App</h1>
-      </header>
-      <PostForm />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React Redux App</h1>
+        </header>
+        <PostForm />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/actions/postAction.js
@@ -0,0 +1,3 @@
+import { FETCH_POSTS, NEW_POSTS } from './types';
+
+export function fectPost() {}
