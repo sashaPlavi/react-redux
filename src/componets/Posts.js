d5cc7789823ff89264f931f242072eb0de8d2b1d
SashaPlavi <sasha.bingulac@gmail.com>
2021-02-13 13:16:46 +0100

    types

--- src/componets/Posts.js
+++ src/componets/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fectPost } from '../actions/postAction';
 
@@ -23,6 +24,11 @@ class Posts extends Component {
     );
   }
 }
+
+Posts.propTypes = {
+  fectPost: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+};
 
 const mapStateToProps = (state) => ({
   posts: state.posts.items,
